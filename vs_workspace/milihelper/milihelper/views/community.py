2ff2e96ee5e9568956a51d068244756dc9f4f26c
jang111 <jangty7@gmail.com>
2024-01-18 20:48:25 +0900

    Fourth_commit

--- vs_workspace/milihelper/milihelper/views/community.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-
-from milihelper.utils.community import get_utils_community
-
-def get_views_community(request):
-    return JsonResponse(data=get_utils_community())
